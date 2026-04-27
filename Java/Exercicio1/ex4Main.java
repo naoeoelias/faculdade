0bc70e844937a36d040d8d761278f35fc209333a
elias <eliassilvestre00@gmail.com>
2026-04-27 10:58:09 -0300

    upload ex4

---
+++ Java/Exercicio1/ex4Main.java
@@ -0,0 +1,11 @@
+// criação da classe principal
+public class ex4Main {
+    public static void main(String[] args){
+
+        // linha de criação do objeto ContaBancaria
+        ex4ContaBancaria c = new ex4ContaBancaria();
+
+        // linha que chama o metodo principal
+        c.operacao();
+    }
+}
